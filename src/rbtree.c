b6dee37fc5aea1300c507dc91ecbeea227ea7d11
chb <chabin7181@gmail.com>
2024-02-06 16:43:20 +0900

    add min/max

--- src/rbtree.c
+++ src/rbtree.c
@@ -181,12 +181,20 @@ node_t *rbtree_find(const rbtree *t, const key_t key) {
 
 node_t *rbtree_min(const rbtree *t) {
   // TODO: implement find
-  return t->root;
+  node_t *current = t->root;
+  while(current->left != t->nil)
+    current = current->left;
+  
+  return current;
 }
 
 node_t *rbtree_max(const rbtree *t) {
   // TODO: implement find
-  return t->root;
+  node_t *current = t->root;
+  while(current->right != t->nil)
+  current = current->right;
+  
+  return current;
 }
 
 int rbtree_erase(rbtree *t, node_t *p) {
